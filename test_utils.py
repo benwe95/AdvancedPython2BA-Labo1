159c083028f98d5ce4f7e3d1d316767e9a7cd5fc
benwe95 <14256@ecam.be>
2016-02-03 15:58:30 +0100

    Fix utils module to pass tests

--- test_utils.py
+++ test_utils.py
@@ -9,13 +9,19 @@ class TestUtils(unittest.TestCase):
     def test_fact(self):
         self.assertEqual(utils.fact(3),6)
         self.assertEqual(utils.fact(4),24)
+        #pour tester si la fonction lance une exception
+        with self.assertRaises(ValueError):
+            utils.fact(-1)
+        
     
     def test_roots(self):
-        self.assertEqual(utils.roots(1,-2,1),(1))
-        self.assertEqual(utils.roots(4,1,-4),(0.5))
+        self.assertEqual(utils.roots(1,-2,1),(1.0))
+        self.assertEqual(utils.roots(4,-4,1),(0.5))
     
     def test_integrate(self):
-        self.assertEqual(utils.integrate(2*x,0,4),8)
+        self.assertTrue(15.8<utils.integrate('2*x',0,4),16<16.1)
+        #Comme on APPROXIME l'intégrale, on ne pourra jamais obtenir la valeur exacte de -4/3
+        self.assertTrue(-1.5<utils.integrate('x**2-1',-1,1)<-1.2)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
+from math import sqrt
 
 def fact(n):
     """Computes the factorial of a natural number.
@@ -10,6 +11,14 @@ def fact(n):
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
+    if n<0:
+        raise ValueError()
+               
+    tot=1
+    for i in range(1,n+1):
+        tot=tot*i
+    return tot
+
 
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -18,7 +27,16 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
+
+    determinant=b**2-4*a*c
+
+    if determinant<0:
+        return (None)
+    elif determinant==0:
+        return (-b/(2*a))
+    else:
+        return ((-b+sqrt(determinant))/(2*a),(-b-sqrt(determinant))/(2*a))
+    
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
@@ -29,9 +47,23 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    pass
+
+    #Fonction eval('  ') pour extraire une fonction mathématique d'un string
+    #x=3   eval(2x+3) -> 9
+
+    total=0
+    pas=0.001
+    while lower<=upper:
+        x=lower
+        total+=pas*eval(function)
+        lower+=pas
+    return total
 
 if __name__ == '__main__':
-    print(fact(5))
-    print(roots(1, 0, 1))
+    print(fact(3))
+    print(roots(1,0,1))
     print(integrate('x ** 2 - 1', -1, 1))
+    print(integrate('x+1',1,2))
+
+
+
